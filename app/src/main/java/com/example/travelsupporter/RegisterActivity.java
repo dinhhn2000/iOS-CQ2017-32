78cd6c71e7e9b205080d2f9cc2e6c1aa50ac8cfa
HoanNguyen <nhoxxu197@gmail.com>
2019-11-10 20:02:09 +0700

    + RegisterRequest

--- app/src/main/java/com/example/travelsupporter/RegisterActivity.java
+++ app/src/main/java/com/example/travelsupporter/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.travelsupporter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -23,6 +24,40 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
 
+        final SharedPreferences sharedPreferences = getSharedPreferences("authentication", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
 
+        final Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("http://35.197.153.192:3000/")
+                .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.build();
+        final Travel_Supporter_Client client = retrofit.create(Travel_Supporter_Client.class);
+
+
+        final EditText password = findViewById(R.id.password);
+        final EditText full_name = findViewById(R.id.fullName);
+        final EditText email = findViewById(R.id.email);
+        final EditText phone = findViewById(R.id.phone);
+        final EditText address = findViewById(R.id.address);
+        final EditText dob = findViewById(R.id.dod);
+        final EditText gender = findViewById(R.id.gender);
+        Button okRegisterInBtn = findViewById(R.id.okRegisterInBtn);
+        Button backRegisterInBtn = findViewById(R.id.backRegisterInBtn);
+        okRegisterInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RegisterRequest registerRequest = new RegisterRequest(password.getText().toString(),full_name.getText().toString(),
+                        email.getText().toString(),phone.getText().toString(),address.getText().toString(),dob.getText().toString(),gender.getText().toString());
+
+            }
+        });
+        backRegisterInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplication(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
